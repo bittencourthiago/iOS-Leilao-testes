7db3eab58acda9a21929df3d74da1dff0e9868fb
bittencourthiago <thiago.bittc@gmail.com>
2021-01-06 15:22:07 -0300

    criando classes para simular um gerador de pagamento

---
+++ Leilao/Models/RepositorioDePagamento.swift
@@ -0,0 +1,19 @@
+//
+//  RepositorioDePagamento.swift
+//  Leilao
+//
+//  Created by Thiago Bittencourt Coelho on 06/01/21.
+//  Copyright © 2021 Alura. All rights reserved.
+//
+
+import Foundation
+
+class RepositorioDePagamento {
+    
+    func salva(_ pagamento:Pagamento) {
+        
+        
+        
+    }
+    
+}
