55184e549ce2ade14a36259b810fa703dc290cfe
bittencourthiago <thiago.bittc@gmail.com>
2021-01-06 10:21:43 -0300

    Começando a implementar testes para encerrar leilão

---
+++ LeilaoTests/EncerradorDeLeilaoTests.swift
@@ -0,0 +1,46 @@
+//
+//  EncerradorDeLeilaoTests.swift
+//  LeilaoTests
+//
+//  Created by Thiago Bittencourt Coelho on 06/01/21.
+//  Copyright © 2021 Alura. All rights reserved.
+//
+
+import XCTest
+@testable import Leilao
+
+class EncerradorDeLeilaoTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+    
+    func testDeveEncerrarLeiloesQueComecaramUmaSemanaAntes() {
+        let formatador = DateFormatter()
+        
+        formatador.dateFormat = "yyyy/MM/dd"
+        
+        guard let dataAntiga = formatador.date(from: "2018/05/09") else { return }
+        
+        let tvLed = CriadorDeLeilao().para(descricao: "TV LED").naData(data: dataAntiga).constroi()
+        let geladeira = CriadorDeLeilao().para(descricao: "Geladeira").naData(data: dataAntiga).constroi()
+        
+        let dao = LeilaoDao()
+        dao.salva(tvLed)
+        dao.salva(geladeira)
+        
+        let encerradorDeLeilao = EncerradorDeLeilao()
+        encerradorDeLeilao.encerra()
+        
+        let leiloesEncerrados = dao.encerrados()
+        
+        XCTAssertEqual(2, leiloesEncerrados.count)
+        XCTAssertTrue(leiloesEncerrados[0].isEncerrado()!)
+        XCTAssertTrue(leiloesEncerrados[1].isEncerrado()!)
+    }
+    
+
+}
